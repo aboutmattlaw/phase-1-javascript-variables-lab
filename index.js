7f6c3d30bab9100c4d99e2ac6bdde4e35775ecb0
Matt <aboutmattlaw@gmail.com>
2021-09-05 15:41:20 -0400

    commit tree 02a94a75091993f086b28b05f76012f9811c4d28

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+
+// Second Lab Attempt 
+
+const companyName = "Scuber"
+let mostProfitableNeighborhood = "Chelsea"
+let companyCeo = "Susan Smith"
+
+
+// First Lab Attempt 
+
+// // Code your solution in this file!
+// const companyName = 'Scuber';
+// let mostProfitableNeighborhood = 'Chelsea';
+// let companyCeo = 'Susan Smith';
